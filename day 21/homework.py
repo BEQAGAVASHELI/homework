79897b45774bea488d26cbfcb23cae7d234466e3
BEQAGAVASHELI <beka261011@gmail.com>
2024-07-13 13:47:53 +0200

    second commit

---
+++ day 21/homework.py
@@ -0,0 +1,18 @@
+#დავალება: ჩამოწერეთ რამოდენიმე ინფუთი, და შემოიტანეთ თქვენი სკოლის ნიშნები, შემდეგ კი გამოიტანეთ ამ რიცხვების საშაუალოარითმეტიკული.
+
+
+
+
+
+num1 = int(input("My School Grades: "))
+
+
+num2 = int(input("My School Grades: "))
+
+
+
+num3 = int(input("My School Grades: "))
+print((num1 + num2 + num3) / 3 )
+
+
+
