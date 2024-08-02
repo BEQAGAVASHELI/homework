49cb0dd6ff4278b4888a313a0e559439e74d413a
BEQAGAVASHELI <beka261011@gmail.com>
2024-08-02 18:14:11 +0200

    second commit

---
+++ day 19/homework.py
@@ -0,0 +1,7 @@
+#installed games 
+games = [ 
+    'soccer' , 'tic tac toe' , 'snake' , 'puzzle' , 
+    'rally' ] 
+    choice = int(input())
+    choice2 = int(input())
+    choice4 = int(input())
